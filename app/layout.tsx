fabfc0cab6081dd4fac80fc75add0337b47cfa0f
vikasgod <godvikas2468@gmail.com>
2026-08-25 19:47:43 +0530

    save map

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
 import Provider from "@/lib/provider";
 import ReduxProvider from "@/redux/reduxProvider";
 import InitUser from "./initUser";
